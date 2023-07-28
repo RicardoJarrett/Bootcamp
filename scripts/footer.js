6b29461e3bda10233d67aa892ca0aefc37dd0464
RicardoJarrett <122801467+RicardoJarrett@users.noreply.github.com>
2023-07-28 02:18:08 +0100

    Scrapped old project- started inverted reality

---
+++ scripts/footer.js
@@ -0,0 +1,2 @@
+document.getElementById("footerDiv").innerHTML = 
+"Created by Ricardo Jarrett - gitlinkhere"
---
+++ scripts/nav.js
@@ -0,0 +1,13 @@
+document.getElementById("navMenu").innerHTML = 
+/*Button link code found below.
+https://www.w3docs.com/snippets/html/how-to-create-an-html-button-that-acts-like-a-link.html
+*/
+"<button type=button class=navButton onclick=window.location.href='/index.html';>Home</button>" + 
+"<button type=button class=navButton onclick=window.location.href='/inverted_reality.html';>Inverted Reality</button>" + 
+"<button type=button class=navButton onclick=window.location.href='/the_inverter_strikes_back.html';>The Inverter Strikes Back</button>" + 
+"<button type=button class=navButton onclick=window.location.href='/rise_of_revenge.html';>Rise of Revenge</button>" + 
+"<button type=button class=navButton onclick=window.location.href='/the_inverter_origins.html';>The Inverter: Origins</button>" + 
+"<button type=button class=navButton onclick=window.location.href='/the_inverter_rotmm.html';>The Inverter: Rise of the Mech-Mermaids</button>" + 
+"<button type=button class=navButton onclick=window.location.href='/inverted_unison.html';>Inverted Unison</button>" + 
+"<button type=button class=navButton onclick=window.location.href='/inverted_destiny.html';>Inverted Destiny: The Final Confrontation</button>" + 
+"<button type=button class=navButton onclick=window.location.href='/inverted_moonlight.html';>Inverted Moonlight: Captain Alex's Nightfall</button>"
